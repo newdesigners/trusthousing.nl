204d8c845b29b2d6076dd70183451ed90b01cff9
Steven Wu <master_msp1@hotmail.com>
2021-11-26 13:30:39 +0100

    initial setup

---
+++ pages/index.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="container">
+    <div>
+      <Logo />
+      <h1>trusthousing.nl</h1>
+      <div>
+        <h2>Hello world</h2>
+        <h3>Hello world</h3>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.container {
+  margin: 0 auto;
+  min-height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  text-align: center;
+}
+</style>
