a15f0e8de537201163f2df65f4791a18819b0929
Agency <agency@hhsdtp5134simac.wireless.hhs.nl>
2021-11-28 11:48:10 +0100

    added description

--- nuxt.config.js
+++ nuxt.config.js
@@ -4,14 +4,14 @@ export default {
 
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
-    title: 'TrustHousing',
+    title: 'TrustHousing - we bring back trust in the student housing market',
     htmlAttrs: {
       lang: 'en'
     },
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' }
+      { hid: 'description', name: 'description', content: 'we bring back trust in the student housing market' }
     ],
     link: [
       { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
